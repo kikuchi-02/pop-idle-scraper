3953a2cb6d08692526aa98b9c633a094cf38ac23
kikuchi <pictures1682@gmail.com>
2021-04-18 19:32:05 +0900

    init frontend

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+    'prettier',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    "require-jsdoc": 0
+  },
+};
--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,21 +1,3 @@
-export default {
-  'env': {
-    'es2021': true,
-    'node': true,
-  },
-  'extends': [
-    'google',
-    'prettier',
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 12,
-    'sourceType': 'module',
-  },
-  'plugins': [
-    '@typescript-eslint',
-  ],
-  'rules': {
-    "require-jsdoc": 0
-  },
+module.exports={
+  ...require('../.eslintrc.js')
 };
