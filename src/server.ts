b44831e46d5484432f1d0372cf4c17875c9be392
kikuchi-02 <pictures1682@gmail.com>
2021-09-11 10:25:59 +0900

    refactor

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ createConnection()
   .then((connection) => {
     const app = express();
 
-    const port = 3000;
+    const port = process.env.PORT || 3000;
     app.set('port', port);
 
     app.use(express.urlencoded({ extended: true }));
